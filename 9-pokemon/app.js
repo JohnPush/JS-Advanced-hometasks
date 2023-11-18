f547d761cafef2c665b84b91ae66ee5b92fcb4a8
Evgenii Pushkarev <pushinbrazil@gmail.com>
2023-11-18 17:56:35 -0300

    Add 9-pokemon

---
+++ 9-pokemon/app.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const request = new XMLHttpRequest();
+request.open('GET', 'https://pokeapi.co/api/v2/pokemon/ditto');
+request.send();
+
+request.addEventListener('load', function () {
+    const { abilities } = JSON.parse(this.responseText);
+    const abilityUrl = abilities[0].ability.url;
+	console.log(abilities);
+	console.log(abilityUrl);
+
+    const abilityRequest = new XMLHttpRequest();
+    abilityRequest.open('GET', abilityUrl);
+    abilityRequest.send();
+
+    abilityRequest.addEventListener('load', function () {
+        const abilityData = JSON.parse(this.responseText);
+        const description = abilityData.effect_entries[1].effect;
+        console.log(abilityData);
+        console.log(description);
+    });
+});
