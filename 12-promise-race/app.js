35f59b973cd231e230c8fa5d82b625b6637b88d6
Evgenii Pushkarev <pushinbrazil@gmail.com>
2023-11-20 18:18:34 -0300

    Add 12-promise-race

---
+++ 12-promise-race/app.js
@@ -0,0 +1,27 @@
+'use strict';
+
+function race(promises) {
+  return new Promise((resolve, reject) => {
+    promises.forEach((promise) => {
+      promise.then((result) => {
+        resolve(result);
+      })
+      // О
+      .catch((error) => {
+        reject(error);
+      });
+    });
+  });
+}
+
+const promise1 = new Promise((resolve) => setTimeout(() => resolve('Promise 1 resolved'), 1000));
+const promise2 = new Promise((resolve, reject) => setTimeout(() => reject('Promise 2 rejected'), 500));
+const promise3 = new Promise((resolve) => setTimeout(() => resolve('Promise 3 resolved'), 1500));
+
+race([promise1, promise2, promise3])
+  .then((result) => {
+    console.log('First promise resolved or rejected:', result);
+  })
+  .catch((error) => {
+    console.error('All promises rejected:', error);
+  });
