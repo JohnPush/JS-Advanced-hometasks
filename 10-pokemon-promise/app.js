b5c670eef1bc9b85d92e477576c59a11a0affc2f
Evgenii Pushkarev <pushinbrazil@gmail.com>
2023-11-22 14:18:08 -0300

    corrections 10-pokemon-promise

--- 10-pokemon-promise/app.js
+++ 10-pokemon-promise/app.js
@@ -2,30 +2,41 @@
 
 
 function getData(url, errorMessage, method = 'GET') {
-    return fetch(url, {
-        method,
-    })
-    .then(response => {
-        if (!response.ok) {
-            throw new Error(`${errorMessage} ${response.status}`);
-        }
-        return response.json();
+    return new Promise((resolve, reject) => {
+        fetch(url, {
+            method,
+        })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`${errorMessage} ${response.status}`);
+            }
+            return response.json();
+        })
+        .then(data => {
+            const { abilities } = data;
+            const abilityUrl = abilities[0].ability.url;
+            console.log(abilities);
+            console.log(abilityUrl);
+
+            return fetch(abilityUrl, { method: 'GET' });
+        })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`Can not get ability data ${response.status}`);
+            }
+            return response.json();
+        })
+        .then(data => {
+            const description = data.effect_entries[1].effect;
+            console.log(data);
+            console.log(description);
+            resolve(data);
+        })
+        .catch(error => {
+            console.error('Error:', error.message);
+            reject(error);
+        });
     });
 }
 
 getData('https://pokeapi.co/api/v2/pokemon/ditto', 'Can not get Pokemon data')
-    .then(data => {
-        const { abilities } = data;
-        const abilityUrl = abilities[0].ability.url;
-        console.log(abilities);
-        console.log(abilityUrl);
-        return getData(abilityUrl, 'Can not get ability data');
-    })
-    .then(data => {
-        const description = data.effect_entries[1].effect;
-        console.log(data);
-        console.log(description);
-    })
-    .catch(error => {
-        console.error('Error:', error.message);
-    });
