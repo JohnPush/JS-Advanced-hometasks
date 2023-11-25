0bfdd218fa67f0382205b09682d5953431997f3f
Evgenii Pushkarev <pushinbrazil@gmail.com>
2023-11-25 18:16:37 -0300

    Add 14-user-task

---
+++ 14-user-task/user.js
@@ -0,0 +1,18 @@
+'use strict';
+
+/* Создать 3 файла:
+- user.js - содержит класс User, который в constructor принимает Task и метод do(), который вызывает у Task метод run().
+- task.js - содержит класс Task с методом run(), который выводит в консоль заданной в constructor сообщение.
+- index.js - создает Task, а затем User с этим Task и вызывает у User do()
+*/
+
+
+export class User {
+    constructor(task) {
+    this.task = task;
+  }
+  
+  do() {
+    this.task.run();
+  }
+}
